4a479dca69e854a5daef00efc845cf74dfcf13ae
NatanColleoni <62671225+NatanColleoni@users.noreply.github.com>
2024-09-05 21:02:53 -0300

    #21440 Correção na validação do token (#2)

--- src/Unirota.Application/Commands/Usuarios/EditarUsuarioCommand.cs
+++ src/Unirota.Application/Commands/Usuarios/EditarUsuarioCommand.cs
@@ -6,8 +6,8 @@ namespace Unirota.Application.Commands.Usuarios;
 public class EditarUsuarioCommand : IRequest<UsuarioViewModel>
 {
     public int Id { get; set; }
-    public string Nome { get; set; }
+    public string? Nome { get; set; }
     public DateTime? DataNascimento { get; set; }
-    public string Senha { get; set; }
-    public string ImagemUrl { get; set; }
+    public string? Senha { get; set; }
+    public string? ImagemUrl { get; set; }
 }

55c2d55f59b3562785b5849cdbbe5c5714aebe32
Natan Colleoni <natan.colleoni@coretecnologia.net.br>
2024-09-29 10:29:20 -0300

    Remove interface obsoleta

--- src/Unirota.Application/Specifications/Grupos/ConsultarGrupoPorIdSpec.cs
+++ src/Unirota.Application/Specifications/Grupos/ConsultarGrupoPorIdSpec.cs
@@ -2,12 +2,11 @@ using Ardalis.Specification;
 
 namespace Unirota.Application.Specifications.Grupos;
 
-public class ConsultarGrupoPorIdSpec : Specification<Domain.Entities.Grupos.Grupo>, ISingleResultSpecification
+public class ConsultarGrupoPorIdSpec : Specification<Domain.Entities.Grupos.Grupo>
 {
     public ConsultarGrupoPorIdSpec(int id)
     {
         Query
-            .Where(grupo => grupo.Id == id)
-            .AsNoTracking();
+            .Where(grupo => grupo.Id == id);
     }
 }

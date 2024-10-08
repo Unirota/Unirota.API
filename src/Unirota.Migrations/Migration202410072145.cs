734dba424c2f7a4d15adfc71ed49b99193b814bf
Natan Colleoni <natancolleonifonseca@gmail.com>
2024-10-07 21:53:03 -0300

    22256 Criação de migration de enderecos

---
+++ src/Unirota.Migrations/Migration202410072145.cs
@@ -0,0 +1,26 @@
+﻿using FluentMigrator;
+using Unirota.Migrations.Configuration;
+
+namespace Unirota.Migrations;
+
+[Migration(202410072145)]
+public class Migration202410072145 : Migration
+{
+    public override void Up()
+    {
+        Create.Table("Enderecos")
+            .EntityBase()
+            .WithColumn("UsuarioId").AsInt32().ForeignKey("Usuarios", "Id")
+            .WithColumn("CEP").AsString().NotNullable()
+            .WithColumn("Logradouro").AsString().NotNullable()
+            .WithColumn("Numero").AsInt32().NotNullable()
+            .WithColumn("Cidade").AsString().NotNullable()
+            .WithColumn("Bairro").AsString().NotNullable()
+            .WithColumn("Complemento").AsString().Nullable();
+    }
+
+    public override void Down()
+    {
+        Delete.Table("Enderecos");
+    }
+}

2a0c22ee9a7b127ec363eba107155a1ea9c00421
Natan Colleoni <natancolleonifonseca@gmail.com>
2024-10-07 22:02:37 -0300

    #22256 Criação entidade de dominio endereço

--- src/Unirota.Infrastructure/Persistence/Context/UnirotaDbContext.cs
+++ src/Unirota.Infrastructure/Persistence/Context/UnirotaDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Unirota.Domain.Entities.Covites;
+using Unirota.Domain.Entities.Enderecos;
 using Unirota.Domain.Entities.Grupos;
 using Unirota.Domain.Entities.SolicitacoesDeEntrada;
 using Unirota.Domain.Entities.Usuarios;
@@ -13,8 +14,8 @@ public class UnirotaDbContext(DbContextOptions options) : DbContext(options)
     public DbSet<Usuario> Usuarios => Set<Usuario>();
     public DbSet<Convite> Convites => Set<Convite>();
     public DbSet<Grupo> Grupos => Set<Grupo>();
-    public DbSet<UsuariosGrupo> UsuariosGrupos => Set<UsuariosGrupo>();
     public DbSet<SolicitacaoDeEntrada> SolicitacaoDeEntrada => Set<SolicitacaoDeEntrada>();
     public DbSet<UsuariosGrupo> UsuariosGrupo => Set<UsuariosGrupo>();
     public DbSet<Veiculo> Veiculos => Set<Veiculo>();
+    public DbSet<Endereco> Enderecos => Set<Endereco>();
 }

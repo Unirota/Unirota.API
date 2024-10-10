75396086f270314da388e8edb6d0ac8e502b96e0
MiessiGomes <josericardomgomes@gmail.com>
2024-10-10 20:28:15 -0300

    Features/22375 - Criação e implementação coluna destinono do grupo

--- src/Unirota.Migrations/Migration202410101934.cs
+++ src/Unirota.Migrations/Migration202410101934.cs
@@ -1,6 +1,21 @@
+using FluentMigrator;
+
 namespace Unirota.Migrations;
 
-public class Migration20241010
+[Migration(202410101934)]
+public class Migration202410101934:Migration
 {
-    
+    public override void Up()
+    {
+        Alter.Table("Grupos")
+            .AddColumn("Destino")
+            .AsString()
+            .NotNullable()
+            .WithDefaultValue("");
+    }
+
+    public override void Down()
+    {
+        Delete.Column("Destino").FromTable("Grupos");
+    }
 }

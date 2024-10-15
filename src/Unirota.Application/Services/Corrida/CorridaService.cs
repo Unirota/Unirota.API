f7143b66530cf847f4aa24450b07705ba710e1af
Maria Dutra <ra-21174887-2@alunos.unicesumar.edu.br>
2024-10-14 21:52:23 -0300

    Criado endpoint para retornar todas as corridas de um grupo através do ID do grupo

--- src/Unirota.Application/Services/Corrida/CorridaService.cs
+++ src/Unirota.Application/Services/Corrida/CorridaService.cs
@@ -1,16 +1,22 @@
 ﻿using Unirota.Application.Commands.Corridas;
 using Unirota.Application.Persistence;
+using Unirota.Application.Queries.Corrida;
+using Unirota.Application.Specification.Corridas;
+using Unirota.Application.Specifications.Grupos;
+using Unirota.Domain.Entities.Corridas;
 
 namespace Unirota.Application.Services.Corrida;
 public class CorridaService : ICorridaService
 {
     private readonly IRepository<Domain.Entities.Corridas.Corrida> _repository;
+    private readonly IReadRepository<Domain.Entities.Corridas.Corrida> _readrepository;
     private readonly IServiceContext _serviceContext;
 
-    public CorridaService(IRepository<Domain.Entities.Corridas.Corrida> repository, IServiceContext serviceContext)
+    public CorridaService(IRepository<Domain.Entities.Corridas.Corrida> repository, IServiceContext serviceContext, IReadRepository<Domain.Entities.Corridas.Corrida> readRepository)
     {
         _repository = repository;
         _serviceContext = serviceContext;
+        _readrepository = readRepository;
     }
 
     public async Task<int> Criar(CriarCorridaCommand dto)
@@ -19,5 +25,15 @@ public class CorridaService : ICorridaService
 
         await _repository.AddAsync(corrida);
         return corrida.Id;
+    }
+
+    public async Task<List<Domain.Entities.Corridas.Corrida?>> ObterPorIdDeGrupo(ConsultarCorridaPorIdQuery request, CancellationToken cancellationToken)
+    {
+        var corridas = await _readrepository.ListAsync(new ConsultarCorridaPorIdSpec(request.Id), cancellationToken);
+        
+        if(corridas == null)
+            _serviceContext.AddError("Não existe corridas para este grupo");
+        return corridas;
+
     }
 }

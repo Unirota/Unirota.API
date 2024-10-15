712298ec36baac3b170f2897efbc2a72e1fbb007
NatanColleoni <62671225+NatanColleoni@users.noreply.github.com>
2024-10-15 18:48:57 -0300

    #22136 CRIAR CAMADA DE TESTES PARA O PROJETO (#26)
    * #22136 CRIAR CAMADA DE TESTES PARA O PROJETO
    
    * #22136 CRIAR CAMADA DE TESTES PARA O PROJETO
    
    ---------
    
    Co-authored-by: Natan Colleoni <natan.colleoni@coretecnologia.net.br>

---
+++ tests/Unirota.UnitTests/Builder/UsuarioBuilder.cs
@@ -0,0 +1,49 @@
+﻿using Bogus;
+using Unirota.Domain.Entities.Enderecos;
+using Unirota.Domain.Entities.Usuarios;
+
+namespace Unirota.UnitTests.Builder;
+
+public class UsuarioBuilder
+{
+    private string _cpf;
+    private Endereco _endereco;
+
+    public Usuario Build()
+    {
+        return Build(1).First();
+    }
+
+    public ICollection<Usuario> Build(int count)
+    {
+        var faker = new Faker<Usuario>("pt_BR")
+                .CustomInstantiator(f =>
+                {
+                    var nome = f.Name.FullName();
+                    var email = f.Internet.Email(nome);
+                    var senha = f.Internet.Password();
+                    var dataNascimento = f.Date.Recent(365);
+                    
+                    var usuario = new Usuario(nome, email, senha, _cpf, dataNascimento);
+                    var endereco = new EnderecoBuilder().WithUsuarioId(usuario.Id).Build();
+
+                    usuario.AlterarEndereco(_endereco ?? endereco);
+
+                    return usuario;
+                });
+
+        return faker.Generate(count);
+    }
+
+    public UsuarioBuilder WithCPF(string cpf)
+    {
+        _cpf = cpf;
+        return this;
+    }
+
+    public UsuarioBuilder WithEndereco(Endereco endereco)
+    {
+        _endereco = endereco;
+        return this;
+    }
+}
